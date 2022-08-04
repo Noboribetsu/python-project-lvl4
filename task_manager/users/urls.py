22ff677dfce81f2f3d67315ccf26c542b961fd1e
Vova <bkj@ya.ru>
2022-08-04 10:39:10 +0300

    fix user/create url

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('users/', UsersList.as_view(), name='users'),
     path('users/<int:pk>/update/', UserUpdate.as_view(), name='update_user'),
     path('users/<int:pk>/delete/', UserDelete.as_view(), name='delete_user'),
-    path('create/', CreateUser.as_view(), name='create_user'),
+    path('users/create/', CreateUser.as_view(), name='create_user'),
     path('login/', AuthUser.as_view(), name='login'),
     path('logout/', LogOutUser.as_view(), name='logout'),
 ]
