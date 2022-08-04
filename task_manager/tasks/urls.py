d25bcc1f04f28aa771a2f089fabb9ef2b112bb19
Vova <bkj@ya.ru>
2022-08-04 11:54:21 +0300

    fix url routes

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from task_manager.tasks.views import CreateTask, TasksList, UpdateTask, DeleteTask, DetailTask
 
 urlpatterns = [
-    path('tasks', TasksList.as_view(), name='tasks'),
-    path('tasks/create', CreateTask.as_view(), name='create_task'),
-    path('tasks/<int:pk>', DetailTask.as_view(), name='task_details'),
+    path('tasks/', TasksList.as_view(), name='tasks'),
+    path('tasks/create/', CreateTask.as_view(), name='create_task'),
+    path('tasks/<int:pk>/', DetailTask.as_view(), name='task_details'),
     path('tasks/<int:pk>/update/', UpdateTask.as_view(), name='update_task'),
     path('tasks/<int:pk>/delete/', DeleteTask.as_view(), name='delete_task'),
 ]
